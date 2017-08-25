9b6f288fe50a17b48c102ee9a2daecb699efc276
Ray Donnelly <mingw.android@gmail.com>
2017-08-25 13:12:42 +0100

    Fix cross-compilation support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
-./configure --prefix=$PREFIX --without-snapshot
+./configure --prefix=$PREFIX --cross-compiling --without-snapshot
 make -j${CPU_COUNT}
 make install
 
