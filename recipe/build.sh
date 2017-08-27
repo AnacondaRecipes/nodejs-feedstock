6befe907794b2a3cabfdac9c787afdfbd9a57eb9
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 01:37:34 +0100

    Generalize logging

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 # The without snapshot comes from the error in
 # https://github.com/nodejs/node/issues/4212.
 ./configure --prefix=$PREFIX --cross-compiling --without-snapshot
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
 
 node -v
