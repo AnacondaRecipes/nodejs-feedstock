097c68b0f6e196b38b48a83e1973b5eeedb57162
Kai Tietz <ktietz@anaconda.com>
2023-04-13 15:13:28 +0200

    adjust build for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,8 @@ if [ "$(uname)" = "Darwin" ]; then
     export CPPFLAGS="$(echo ${CPPFLAGS:-} | sed -E 's@\-mmacosx\-version\-min=[^ ]*@@g')"
     export CPPFLAGS="${CPPFLAGS} -D_DARWIN_C_SOURCE"
     echo "CPPFLAGS=$CPPFLAGS"
+    ln -s $BUILD_PREFIX/bin/llvm-libtool-darwin ./libtool
+    export PATH=${PWD}:${PATH}
 else
     # need librt for clock_gettime with nodejs >= 12.12
     export LDFLAGS="$LDFLAGS -lrt"
